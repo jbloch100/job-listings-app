93553f23a5e86b6ca0c78e32676cf6e393db27f6
Jonathan Bloch <jonathanbloch100@gmail.com>
2025-01-09 14:49:52 -0500

    Fixed broken California link in API job descriptions

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -2,20 +2,14 @@ export async function fetchJobs() {
   const response = await fetch('https://remotive.io/api/remote-jobs');
   let data = await response.json();
   
-  // Debugging: Log first job description
-  console.log("Original Job Description:", data.jobs[0].description);
-  
-  // Fix broken California link
+  // Replace broken Suvoda California privacy link with a real working link
   data.jobs = data.jobs.map(job => ({
   ...job,
   description: job.description.replace(
-  /https:\/\/www\.suvoda\.com\/california-applicant-privacy-notice-and-policy/g, // Regex to catch variations
-  'https://correct-california-info.com' // Replace with a valid link
+  'https://www.suvoda.com/california-applicant-privacy-notice-and-policy',
+  'https://policies.google.com/privacy?hl=en-US' // Replace with actual working link
   )
   }));
-  
-  // Debugging: Log fixed description
-  console.log("Updated Job Description:", data.jobs[0].description);
   
   return data;
   }
